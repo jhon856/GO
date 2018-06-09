5ea0246931a47b9f60d56f17fcfefc187c4610ba
jhonnathan henriquez <jhonnathan.henriquez@gmail.com>
2018-06-09 15:28:33 -0400

    template nivel intemedio

--- Template/template1.go
+++ Template/template1.go
@@ -7,17 +7,37 @@ import (
 	"net/http"
 )
 
+//Usuario algo
+type Usuario struct {
+	UserName string
+	Edad     int
+	Activo   bool
+	Admin    bool
+	Tags     []string
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// este es para mandar un template	///template, err := template.New("Hola").Parse("hola este es un template")// de este forma se manda a la pagina html
-		template, err := template.ParseFiles("./index.html") // este es si le pasas un arch Html
+		template, err := template.ParseFiles("./index.html") // este  si le pasas un arch Html
 		// metodo ParseFiles para pasar un archivo HTML
+
+		//otro comandoo
+		/*
+			ParseGlob("templates/*.gohtml")
+		*/
 		if err != nil {
 			fmt.Println("error")
 			panic(err)
 		}
-		template.Execute(w, nil)
+		tags := []string{"GO", "Java", "Angular"}
+		usuario := Usuario{UserName: "Jhonnathan Henriquez",
+			Edad:   26,
+			Activo: true,
+			Admin:  false,
+			Tags:   tags}
+		template.Execute(w, usuario)
 	})
 
 	fmt.Println("el servidos escucha en el puerto :8000")
